655741c545248d00e252042bde0bcb9f76d02acf
ヨシダタイキ <abc521fjr7@gmail.com>
2021-05-02 19:47:01 +0900

    [add]アクション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness:true, length: {in: 2..20}
-  validates :introduction,length: { maximum: 50}
-
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-  attachment :profile_image
-  has_many :books, dependent: :destroy
+         :recoverable, :rememberable, :validatable
 end
